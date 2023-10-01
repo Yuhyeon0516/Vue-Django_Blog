ae0adcc51b66a84188caa16f4df2a379bc1b53f4
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-01 12:56:34 +0900

    Applied taggit and added tags in Post model.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "taggit",
     "blog.apps.BlogConfig",
 ]
 
@@ -124,3 +125,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+TAGGIT_CASE_INSENSITIVE = True
