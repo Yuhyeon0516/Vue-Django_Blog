9f1df16dfd42f3686ac2c4cb6f0aefb7f4d274fd
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-04 11:58:58 +0900

    Set pages in vue for MPA use.

---
+++ frontend/src/pages/AppPostList.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app clipped>
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-view-dashboard</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Dashboard</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-cog</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Settings</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app clipped-left color="indigo" dark>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Blog</v-app-bar-title>
+        </v-app-bar>
+
+        <v-main>
+            <PostList />
+        </v-main>
+
+        <v-footer app>
+            <span>&copy; 2023</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PostList from '../components/PostList.vue';
+
+const drawer = ref(null);
+</script>
+
+<script>
+export default {
+    components: { PostList },
+    data: () => ({
+        drawer: null,
+    }),
+};
+</script>
