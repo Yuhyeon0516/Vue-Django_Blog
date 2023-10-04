cd92adee8bda6b3d4e31d79ab3354e39237d5e72
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-04 14:19:41 +0900

    Separated app bar  to common component

--- frontend/src/pages/AppPostDetail.vue
+++ frontend/src/pages/AppPostDetail.vue
@@ -1,64 +1,6 @@
 <template>
     <v-app id="inspire">
-        <v-navigation-drawer v-model="drawer" app clipped>
-            <v-list dense>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-view-dashboard</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Dashboard</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-cog</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Settings</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-
-        <v-app-bar app clipped-left color="indigo" dark>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-            <v-toolbar-title>Vue.js - Django Web Blog</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-            <v-btn text href="/">Home</v-btn>
-            <v-btn text href="/post_list.html">POSTLIST</v-btn>
-            <v-btn text href="/post_detail.html">POSTDETAIL</v-btn>
-
-            <v-spacer></v-spacer>
-
-            <v-menu offset-y left bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn text v-bind="attrs" v-on="on">
-                        <v-icon>mdi-account</v-icon>Anonymous
-                        <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
-                </template>
-
-                <v-list>
-                    <v-list-item>
-                        <v-list-item-title>Login</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-title>Register</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-title>Logout</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-title>Password Change</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-        </v-app-bar>
-
+        <MainMenu />
         <v-main>
             <PostDetail />
         </v-main>
@@ -70,17 +12,14 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import PostDetail from '../components/PostDetail.vue';
+import MainMenu from '@/components/MainMenu.vue';
 
-const drawer = ref(null);
+import PostDetail from '../components/PostDetail.vue';
 </script>
 
 <script>
 export default {
-    components: { PostDetail },
-    data: () => ({
-        drawer: null,
-    }),
+    components: { PostDetail, MainMenu },
+    data: () => ({}),
 };
 </script>
