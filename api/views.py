4d6e08e4417a17e6ce835203478bc79f2cbd1b32
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-04 17:44:57 +0900

    Created function in Django that getting post detail infomation.

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
-from django.views.generic import ListView
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.list import BaseListView
 from api.views_util import obj_to_post
 
 from blog.models import Post
 
 
 # Create your views here.
-class ApiPostLV(ListView):
+class ApiPostLV(BaseListView):
     model = Post
 
     def render_to_response(self, context, **response_kwargs):
@@ -14,3 +15,13 @@ class ApiPostLV(ListView):
         postList = [obj_to_post(obj) for obj in querySet]
 
         return JsonResponse(data=postList, safe=False, status=200)
+
+
+class ApiPostDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context["object"]
+        post = obj_to_post(obj)
+
+        return JsonResponse(data=post, safe=True, status=200)
